1f89f90b9d1b25365dc1eb39b4acdafe4e4b5722
MattThompson-059 <mattthompson.059@gmial.com>
2023-03-21 15:44:15 -0400

    added foundation for sql_script

--- sql_connection/sql_script.py
+++ sql_connection/sql_script.py
@@ -1,1 +1,4 @@
-user_table = "create table User (UserID int, Username char(45), Password VARCHAR(10),
+user_table = "create table User (UserID int, Username varchar(20), primary key (Username), Password VARCHAR(15), height float, weight float, experience varchar(10), age int (2);"
+
+goal_table = "create table Goal (OneRepMax int(3), ReachDate date, foreign key (UserID) references User(UserID);"
+
