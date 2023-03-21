e6b8da95a24570231486b5051f8cc208ea08a01d
MattThompson-059 <mattthompson.059@gmial.com>
2023-03-21 16:07:59 -0400

    refined the foundation of it a tad more

--- sql_connection/sql_script.py
+++ sql_connection/sql_script.py
@@ -1,4 +1,7 @@
-user_table = "create table User (UserID int, Username varchar(20), primary key (Username), Password VARCHAR(15), height float, weight float, experience varchar(10), age int (2);"
+user_table = "create table User (UserID int, Username varchar(20), primary key (Username), Password VARCHAR(15), height float, weight float, experience varchar(10), age int (2));"
+routine_table = "create table Routine(UserID int, days int(5), sets int, reps int, foreign key (UserID) references User(UserID));"
+goal_table = "create table Goal (UserID int, OneRepMax int(3), ReachDate date, foreign key (UserID) references User(UserID));"
+exercises_table = "create table Exercises (UserID int, CurrentMax int not null auto_increment, biceps varchar, triceps varchar, abs varchar, chest varchar, back varchar, legs varchar, primary key(CurrentMax), foreign key (UserID) references User(UserID));" 
 
-goal_table = "create table Goal (OneRepMax int(3), ReachDate date, foreign key (UserID) references User(UserID);"
-
+# min weight 4.7 lbs, max weight 1400 lbs.
+# min height 2'1" in, max height 8'11" in.
